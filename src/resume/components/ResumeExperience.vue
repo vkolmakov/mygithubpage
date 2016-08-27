6324944270bc473fa0e41ccb4d892fb5e7f45ec2
vkolmakov <vkolmakov@hotmail.com>
2016-08-27 12:37:10 -0500

    Update experience section styles

--- src/resume/components/ResumeExperience.vue
+++ src/resume/components/ResumeExperience.vue
@@ -4,8 +4,8 @@
     <div class="job" v-for="job in jobs">
       <div class="info">
         <div class="company"><strong>{{ job.company }}</strong>, {{ job.city }}</div>
-        <div class="dates">{{ job.dates }}</div>
         <div class="title">{{ job.title }}</div>
+        <div class="dates">{{ job.dates }}</div>
       </div>
       <ul>
           <li v-for="bulletpoint in job.bulletpoints">
@@ -29,18 +29,34 @@
 <style lang="sass">
   @import "../mixins";
 
+  @mixin info-md() {
+   .company {
+      @include left-col();
+    }
+    .title {
+      @include left-col();
+    }
+    .dates {
+      @include right-col();
+    }
+  }
+
+  @media print {
+    div.experience > .job > .info {
+      @include info-md();
+    }
+  }
+
   .experience {
     .job {
       .info {
         @include clearfix();
-        .company {
-          @include left-col();
-        }
         .title {
-          @include left-col(true);
+          font-style: italic;
+          font-weight: 500;
         }
-        .dates {
-          @include right-col();
+        @include breakpoint-md() {
+          @include info-md();
         }
       }
 
