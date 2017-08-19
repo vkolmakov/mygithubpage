6481c5da6b1622edafb4da376f26ddfb6614098f
vkolmakov <vkolmakov@hotmail.com>
2017-08-19 00:56:23 -0500

    Update resume contents

--- src/resume/Resume.vue
+++ src/resume/Resume.vue
@@ -71,12 +71,22 @@
             city: 'Chicago, IL',
             major: 'Bachelor of Science: Computer Science',
             graduation: 'Expected Graduation: December 2017',
-            gpa: '3.9/4.0',
+            gpa: '3.86/4.0',
           }],
         },
 
         experience: {
           jobs: [{
+            company: 'kCura',
+            city: 'Chicago, IL',
+            title: 'Software Engineering Intern',
+            dates: 'June 2017 - present',
+            bulletpoints: [
+              'Improved developer experience by adding incremental builds and fully automating build process for the web component library, which resulted in average raw time savings of 92% per build.',
+              'Made a contribution to Choices.js open-source library with accessibility improvements.',
+              'Contributed to multiple projects, including SkateJS-based web component library, Aurelia and AngularJS based applications.'
+            ],
+          }, {
             company: 'Target',
             city: 'Minneapolis, MN',
             title: 'Software Engineering Intern',
@@ -90,7 +100,7 @@
             company: 'Illinois Institute of Technology',
             city: 'Chicago, IL',
             title: 'Teaching Assistant - Data Structures and Algorithms',
-            dates: 'August 2016 - present',
+            dates: 'August 2016 - May 2017',
             bulletpoints: [
               'Hosted lab sessions and assisted students with their programming assignments.',
               'Evaluated student lab assignments.',
@@ -99,7 +109,7 @@
             company: 'Wilbur Wright College',
             city: 'Chicago, IL',
             title: 'Mathematics Tutor',
-            dates: 'May 2014 - present',
+            dates: 'May 2014 - May 2017',
             bulletpoints: [
               'Designed and developed a fully functional web-application that allows students to schedule tutoring appointments online.',
               'Provided academic assistance to assigned students with various mathematics courses (calculus, statistics), either individually or in groups.',
@@ -108,8 +118,8 @@
         },
 
         technicalSkills: {
-          languages: ['JavaScript/Node.js', 'Scala', 'Python', 'Java', 'C'],
-          technologies: ['React', 'Redux', 'AngularJS', 'Vue', 'Express', 'Django', 'PostgreSQL', 'MongoDB', 'Elasticsearch', 'Kibana', 'HTML5', 'CSS3', 'Sass', 'Bootstrap', 'jQuery'],
+          languages: ['JavaScript/Node.js', 'Python', 'Elm', 'Scala', 'Haskell', 'Java', 'C'],
+          technologies: ['React', 'Redux', 'AngularJS', 'Vue.js', 'Express', 'Django', 'PostgreSQL', 'MongoDB', 'Elasticsearch', 'Kibana', 'RxJS', 'Webpack', 'jspm', 'HTML5', 'CSS3', 'Sass', 'jQuery'],
           tools: ['Git', 'zsh', 'IntelliJ', 'Emacs'],
         },
 
@@ -117,24 +127,17 @@
           projects: [{
             name: 'Math Emporium',
             bulletpoints: [
-              'Self-service web-application that allows students to register using their school email address and schedule tutoring appointments directly at Math Emporium\'s calendar. Has complete integration with Google Calendar and serves as a configurable backend for the Academic Scheduler Chrome extension.',
-              'Backend: Node.js, Express, PostgreSQL; front: React, Redux, Sass (Susy + Compass).',
+              'Self-service web application that allows students to login using their school email address and schedule tutoring appointments directly at Math Emporium\'s calendar. Has complete integration with Google Calendar and serves as a configurable backend for the Academic Scheduler Chrome extension.',
+              'Backend: Node.js, Express, PostgreSQL, MongoDB. Frontend: React, Redux, Sass.',
               'Source: <a href="https://github.com/vkolmakov/math-emporium">Github</a> Link: <a href="http://tutoringatwright.com">tutoringatwright.com</a>',
             ],
           }, {
-            name: 'Academic Scheduler',
+            name: 'Face Recognition Attendance System',
             bulletpoints: [
-              'Chrome extension that is currently used at Wright College\'s Math Emporium for scheduling appointments. Eliminated any possibility for overbooking/mis-scheduling.',
-              'Built with AngularJS and Bootstrap.',
-              'Source: <a href="https://github.com/vkolmakov/academic-scheduler-extension">Github</a>',
+              'Web application designed to seamlessly maintain attendance at schools using face recognition.',
+              'Built with Vue.js, RxJS and OpenFace.',
+              'Source: <a href="https://github.com/vkolmakov/Facial-Recognition-Attendance">Github</a>',
             ],
-          }, {
-            name: 'Recommender',
-            bulletpoints: [
-              'A recommender system that implements item-centric form of collaborative filtering on a data source and can make predictions given either a user or an item.',
-              'Written in Scala with Spark',
-              'Source: <a href="https://github.com/vkolmakov/recommender-system">Github</a>'
-            ]
           }]
         }
       }
@@ -214,7 +217,7 @@
     .resume {
       font-size: 12px;
       border: 0;
-      padding: 1.5em 0 0 0;
+      padding: 8px 0 0 0;
       margin: 0 auto;
 
       color: #111;
