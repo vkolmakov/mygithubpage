f66374be69e46b8b0b7f91e88235055a7a3a1224
vkolmakov <vkolmakov@hotmail.com>
2017-08-19 00:55:53 -0500

    Update page title for every route

--- src/main.js
+++ src/main.js
@@ -6,14 +6,19 @@ import Home from './home/Home.vue'
 import Resume from './resume/Resume.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/resume', component: Resume },
+  { path: '/', component: Home, meta: { title: 'Personal Website' } },
+  { path: '/resume', component: Resume, meta: { title: 'Resume' } },
 ]
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `Vladimir Kolmakov - ${to.meta.title}`
+  next()
 })
 
 new Vue({
